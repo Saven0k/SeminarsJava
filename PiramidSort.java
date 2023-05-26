b6e68de84326f3a5cf8ec532818649018a8edc72
Savenok <Romasav2017@gmail.com>
2023-05-26 08:12:55 +0300

    piramidSort

---
+++ PiramidSort.java
@@ -0,0 +1,26 @@
+﻿import java.util.Random;
+
+public class PiramidSort {
+    public static void main(String[] args) {
+        int[] array = new int[100];
+        fillArray(array);
+
+        long StartTime = System.nanoTime();
+        piramidSort(array);
+        System.out.println("Time work: ");
+        System.out.print(System.nanoTime() - StartTime);
+
+
+
+    }
+    public static void fillArray(int [] array){
+        Random r = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = r.nextInt(10000);
+        }
+    }
+
+    public static void piramidSort(int[] array){
+            
+    }
+}

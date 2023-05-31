b726e0d0a869015e5d9861cbac3c74ebdcb006b2
Savenok <Romasav2017@gmail.com>
2023-05-31 08:39:24 +0300

    ReversalList

---
+++ Reversal.java
@@ -0,0 +1,32 @@
+﻿public class Reversal<E>{
+    private Node<E> head;
+
+    public void Revers(E values){
+        Node previous = null;
+        for (E value : values){
+            Node node = new Node();
+            node.setValue(value);
+            if(previous != null){
+                previous.setNext(node);
+            }  else{
+                head = node;
+            }
+            previous = node;
+
+        }
+    }
+
+    public Reversal<E> rev(){
+        Node node = head;
+        Node previous = null;
+
+        while(node != null){
+            Node tmp = node.getNext();
+
+            node.setNext(previous);
+            previous = node;
+            node = tmp;
+        }
+        return this;
+    } 
+}
